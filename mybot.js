20b36c1241bc8e665214451371575fe37e2d88cf
Sher13 <koshmarik13@bk.ru>
2018-12-15 12:32:44 +0300

    ...

--- mybot.js
+++ mybot.js
@@ -18,43 +18,30 @@ client.on("message", (msg) => {
 });
 const config = require('./config.json');
 
-const size = config.colors;
-const rainbow = new Array(size);
-
-for (var i=0; i<size; i++) {
-var red = sin_to_hex(i, 0 * Math.PI * 2/3); // 0 deg
-var blue = sin_to_hex(i, 1 * Math.PI * 2/3); // 120 deg
-var green = sin_to_hex(i, 2 * Math.PI * 2/3); // 240 deg
-
-rainbow[i] = '#'+ red + green + blue;
-}
-
-function sin_to_hex(i, phase) {
-var sin = Math.sin(Math.PI / size * 2 * i + phase);
-var int = Math.floor(sin * 127) + 128;
+function to_hex(i) {
+var int = i;
 var hex = int.toString(16);
-
 return hex.length === 1 ? '0'+hex : hex;
 }
 
-let place = 0;
 const servers = config.servers;
 
 function changeColor() {
 for (let index = 0; index < servers.length; ++index) {
-client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(rainbow[place])
+var g=Math.random()%255;
+var r=Math.random()%255;
+var b=Math.random()%255;
+var zet='#'+to_hex(g)+to_hex(r)+to_hex(b);
+client.guilds.get(servers[index]).roles.find('name', config.roleName).setColor(zet)
     .catch(console.error);
  
 if(config.logging){
-console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+console.log(`[ColorChanger] Changed color to ${zet} in server: ${servers[index]}`);
 }
-if(place == (size - 1)){
-place = 0;
-}else{
-place++;
+
 }
 }
-}
+
 
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.username}!`);
