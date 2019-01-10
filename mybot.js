dd3c08abbfed06a8f076cc27412bd58b58386316
Sher13 <koshmarik13@bk.ru>
2019-01-10 23:52:47 +0300

    ...

--- mybot.js
+++ mybot.js
@@ -27,8 +27,10 @@ if (msg.content.startsWith("ch")) {
 var o =msg.author.id;
 if (msg.content.includes("<@204590199932452864>"))
 	msg.channel.send("Ужасный ужасный человек. Забудь его ник")
-if (msg.content.includes("Вообще")&&o=="532969529857409063")
+if (msg.content.startsWith("Вообще")&&o=="532969529857409063")
 	msg.channel.send("Ну такоооое. А ты знал, что он садист?")
+if (msg.content.startsWith("А ты слышала, что он говорил об этом? Это только эстетика, а не то, без чего нельзя жить!")&&o=="532969529857409063")
+	msg.channel.send("Меня пугает твоя осведомленность в данном вопросе :thinking: ")
 	 if(msg.content.includes("wtf")||(msg.content.includes("fuck"))||(msg.content.includes("bitch"))){
    msg.delete();
    msg.reply("***bad words are disable in server***:rage:");
