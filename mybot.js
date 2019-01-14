30bdae425cd629c0122a0b6a47814e71678777c9
Sher13 <koshmarik13@bk.ru>
2019-01-14 21:27:37 +0300

    /

--- mybot.js
+++ mybot.js
@@ -1,14 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs")
-var cin = fs.readFileSync("file.txt", "utf8");
 const config = require('./config.json');
-var dateTime = require('node-datetime');
-var fl=cin;
-function flag(){
-	fl=1;
-	fs.writeFileSync("file.txt", fl);
-}
  function per(s)
 {
 	var n=s.length;
@@ -42,79 +34,24 @@ if (msg.content==="ch") {
     changeColor();
 	msg.channel.send("ok:ok_hand: :wink: ");
   }
+if (msg.content.includes(" char"))
+	{
+		var mess=msg.content.substring(0,msg.content.indexOf(" char"));
+		var e=per(mess);
+		msg.reply(e);
+	}
 var o =msg.author.id;
+if (msg.content.includes("<@204590199932452864>"))
+	msg.channel.send("Ужасный ужасный человек. Забудь его ник")
+if (msg.content=="Вообще то он классный!!!"&&o=="532969529857409063")
+	msg.channel.send("Ну такоооое. А ты знал, что он садист?")
+if (msg.content=="А ты слышала, что он говорил об этом? Это только эстетика, а не то, без чего нельзя жить!"&&o=="532969529857409063")
+	msg.channel.send("Меня пугает твоя осведомленность в данном вопросе :thinking: ")
 	 if(msg.content.includes("wtf")||(msg.content.includes("fuck"))||(msg.content.includes("bitch"))){
    msg.delete();
    msg.reply("***bad words are disable in server***:rage:");
   }
-var id="465931840398557194";
-var rus="204590199932452864";
-var us=client.users.get(id);
-if (fl==1&&o==id&&us.presence.status=="offline"&&msg.content.includes("<@465931840398557194>"))
-	{
-		msg.reply("Мне жаль. Аля сейчас офлайн");
-		var dt = dateTime.create();
-		var data=new Date(dt.now())+"";
-		var tm=data.substring(16,18);
-		if (tm>=8&&tm<20)
-			{
-				var emb = {
-					embed: {
-					title: "**Скорее всего Аля сейчас на учебе. Ей очень скучно, но видимо сейчас важная пара**",
-					color:"14614685",
-					image:
-						{
-							url: "https://cdn.discordapp.com/attachments/534380967130038274/534427113281814540/nya12.gif"
-						}
-					}
-					};
-				msg.channel.send(emb);
-				
-			}
-		else
-			{
-				var emb = {
-					embed: {
-					title: "**Аля ушла спать.Она дико извинялась и просила передать, что скучает** :heart: ",
-					color:"14614685",
-					image:
-						{
-							url: "https://cdn.discordapp.com/attachments/534380967130038274/534420063013437460/nya13.gif"
-						}
-					}
-					};
-				msg.channel.send(emb);
-			}
-		fl=0;
-	}
-if (msg.content.includes("скушать <@465931840398557194>"))
-	{
-		var emb = {
-					embed: {
-					title: "**Алю нельзя кушать. А Куцкер жёпа. Воть.**\n ",
-					color:"14614685",
-					image:
-						{
-							url: "https://cdn.discordapp.com/attachments/534380967130038274/534421438677909526/nya11.gif"
-						}
-					}
-					};
-				setTimeout(function (){msg.channel.send(emb)},2000);
-	}
-	if (msg.content.includes("Куцкер злой")||msg.content.includes("Куцкер Злой")||msg.content.includes("куцкер Злой")||msg.content.includes("куцкер злой"))
-	{
-		var emb = {
-					embed: {
-					title: "**Ня. Вот не будь злым. Кушай.**\n ",
-					color:"14614685",
-					image:
-						{
-							url: "https://cdn.discordapp.com/attachments/534380967130038274/534424030627889152/nya10.gif"
-						}
-					}
-					};
-				msg.channel.send(emb);
-	}
+
 });
 function to_hex(i) {
 var int = i;
@@ -124,11 +61,14 @@ return hex.length === 1 ? '0'+hex : hex;
 
 const servers = config.servers;
 
+
+
+
 client.on('ready', () => {
 console.log(`Logged in as ${client.user.username}!`);
 if(config.speed < 10){console.log("The minimum speed is 60.000, if this gets abused your bot might get IP-banned"); process.exit(1);}
 setInterval(changeColor, config.speed);
-setInterval(flag, config.kuc);
 });
-fs.writeFileSync("file.txt", fl) ; 
+
+ 
 client.login(process.env.TOKEN);
---
+++ usersRolesBD.js
@@ -0,0 +1,122 @@
+﻿exports.name='userRolesBD:';
+exports.system={
+     DISABLE:false,
+     DATA_BASE_NAME:'test_bd.bd',
+     TABLE_NAME:'users_roles16',
+     SERVER_ID:'437647330406039572'
+};
+exports.run=async(client,message,args)=>{
+   if(message.guild.id!=module.exports.system.SERVER_ID){message.channel.send('Wrong server!'); return;};
+   if(message.content.search('enable')!=-1){module.exports.system.DISABLE=false; message.channel.send(' is enabled now'); return; };
+   if(message.content.search('disable')!=-1){module.exports.system.DISABLE=true; message.channel.send(' is disabled now'); return; };
+   if(module.exports.system.DISABLE) { message.channel.send('disabled!'); return;};
+ 
+   if(message.content.search('test')!=-1) {
+          console.log('usersRoleBD test:' ); 
+          console.log('insert: ');
+          await module.exports.insert(client,message.member);
+          console.log('set: ');
+          await module.exports.set(client,message.member);
+    }else
+
+    if(message.content.search('scan')!=-1){
+       console.log('usersRoleBD scan:' ); 
+        let mmb_arr =await message.guild.fetchMembers().then(members=>{return members;}).catch(err=>{console.error(err);});
+        mmb_arr=mmb_arr.members.array();
+       for(var i = 0; i< mmb_arr.length;i++){
+                   await module.exports.insert(client,mmb_arr[i]);
+        };
+     
+
+    };//
+    
+};//run end
+exports.set = async(client,member)=>{
+      if(member.guild.id!=module.exports.system.SERVER_ID){console.log('Wrong server!');return;};
+      if(module.exports.system.DISABLE) { console.log('disabled!'); return;};
+try{
+
+    let log = (arg) => {console.log(module.exports.name+'set:'+arg);};
+    const fs = require('fs');
+    const sqlite = require('aa-sqlite');
+    const DATA_BASE_NAME=module.exports.system.DATA_BASE_NAME;
+    const TABLE_NAME = module.exports.system.TABLE_NAME;
+    
+    async function process(){
+       
+       try{
+     let resolve='';   
+     await sqlite.open('./$DATA_BASE_NAME').catch(err=>console.log(err));
+    
+     resolve = await sqlite.get(`SELECT * FROM ${TABLE_NAME}  WHERE userID=='${member.user.id}'`).then(row=>{return (row)?row:false;}).catch(err=>{console.log(err);});
+     if(!resolve) return;
+     log('resolve ');
+     console.log(resolve);
+     let arr =  resolve.rolesIDS.split(','); 
+     log('arr '+ arr);
+     let role='';
+     for(var i=0;i<arr.length;i++){
+           
+           role = await member.guild.roles.get(arr[i]);
+           if(role && role.name!='@everyone') await member.addRole(role).then(log(role.id+' '+role.name+' role add to '+member.user.username +' member' )).catch(err=>console.log(err));
+  
+      };//for end
+     return true;
+         }catch(err){console.log(err);};
+     };//process end
+     await  process();
+}catch(err){log(err);};
+};//get end
+
+
+exports.insert = async(client,member)=>{
+      if(member.guild.id!=module.exports.system.SERVER_ID){console.log('Wrong server!');return;};
+      if(module.exports.system.DISABLE) {console.log('disabled!'); return;};
+try{
+    let log = (arg) => {console.log(module.exports.name+'insert:'+arg);};
+    const fs = require('fs');
+    const sqlite = require('aa-sqlite');
+    const DATA_BASE_NAME=module.exports.system.DATA_BASE_NAME;
+    const TABLE_NAME = module.exports.system.TABLE_NAME;
+   
+    async function getRolesIDS(){
+       let rolesIDS = await member.roles.keyArray().join(',');
+       log(' rolesIDS: '+rolesIDS);
+       return rolesIDS;
+   };//getRolesIds end
+
+   async function process(){
+    let rolesIDS = await getRolesIDS(); 
+    let user_rolesIDS=rolesIDS;
+    await sqlite.open('./$DATA_BASE_NAME').catch(err=>console.log(err));
+
+   let resolve=await sqlite.get(`SELECT * FROM ${TABLE_NAME} WHERE userID=='${member.user.id}'`).then(raw=>{ return (raw)?'has':'apsend';}).catch(err=>{console.log(err);});
+   let resolve = await sqlite.get(`SELECT * FROM ${TABLE_NAME} WHERE userID=='${member.user.id}'`).then(raw=>{return (raw)? 'has':'apsend' }).catch(err=>console.log(err);); 
+         await sqlite.get(`SELECT `);  
+
+
+     log(member.user.username +' member '+resolve+' into table'); //
+  if(resolve=='has'){
+    resolve = await sqlite.run(`UPDATE ${TABLE_NAME} SET rolesIDS=='${user_rolesIDS}' WHERE userID=='${member.user.id}'`).catch(err=>{console.log(err);});
+     log(member.user.username + ' member info is updated');
+   }else if(resolve=='absend'){
+     await sqlite.run(`INSERT INTO ${TABLE_NAME} (userID,rolesIDS) VALUES('${member.user.id}','${user_rolesIDS}')`).catch(err=>{console.log(err)});
+     log(member.user.username + ' member info inserted into table');
+   }else{
+     await sqlite.run(`CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (userID TEXT PRIMARY KEY, rolesIDS TEXT)`).then(r=>{return true;}).catch(err=>{console.log(err);});
+     log(' table created');
+     await sqlite.run(`INSERT INTO ${TABLE_NAME} (userID,rolesIDS) VALUES('${member.user.id}','${user_rolesIDS}')`).then(r=>{return true;}).catch(err=>{console.log(err);});  
+     log(member.user.username + ' member info inserted into table');
+    };//else end
+
+    let table = await sqlite.get(`SELECT * FROM ${TABLE_NAME} WHERE userID=='${member.user.id}'`).then(table=>{return table;}).catch(err=>{console.log(err);});
+    log('table: ');
+    console.log(table);
+
+ };//process end
+
+ await process();
+          
+}catch(err){console.log(err)};
+};//exports.insert end
+
