bad918eb6f24b7ae65357f885b50811e7ca57cd3
Sher13 <koshmarik13@bk.ru>
2019-01-14 22:43:20 +0300

    ...

--- mybot.js
+++ mybot.js
@@ -83,7 +83,7 @@ if (fl==1&&o==id&&us.presence.status=="offline"&&msg.content.includes("<@4659318
 			{
 				var emb = {
 					embed: {
-					title: "**Аля ушла спать.Она дико извинялась и просила передать, что скучает** :heart: ",
+					title: "**Аля ушла спать. Она уже скучает. А вообще иди спать** :heart: ",
 					color:"14614685",
 					image:
 						{
