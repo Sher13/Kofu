e9507b121807d1eb81a2f878991dd0c5f6d280d4
Sher13 <koshmarik13@bk.ru>
2019-10-20 17:53:15 +0300

    add new link

--- mybot.js
+++ mybot.js
@@ -293,16 +293,24 @@ client.on("message", (msg) => {
 		let emb = new RichEmbed()
 			.setColor(14614685)
 			.setTitle("Links")
-			.setDescription(
+			.addField("**_____________**",
 				"[:cherry_blossom:](https://icpc-neerc.atlassian.net/wiki/spaces/2019/pages/293634127) Волонтерство (инфа)\n" +
 				"[:cherry_blossom:](https://neerc.ifmo.ru/volunteers/year/6) Волонтерство (распределение)\n" +
 				"[:cherry_blossom:](https://beautifier.io/) CodeStyle (JS)\n" +
 				"[:cherry_blossom:](http://format.krzaq.cc/) CodeStyle (C++)\n" +
 				"[:cherry_blossom:](http://detexify.kirelabs.org/classify.html) Latex (символы)\n" +
-				"[:cherry_blossom:](https://telegra.ph/STEP-Internship-Impressions-08-05) Стажировка (текст) \n" +
+				"[:cherry_blossom:](https://telegra.ph/STEP-Internship-Impressions-08-05) Стажировка (текст) \n")
+		    .addField("**_____________**",
 				"[:cherry_blossom:](https://careers.google.com/jobs/results/129125924734935750-student-training-in-engineering-program-step-2020/?category=DATA_CENTER_OPERATIONS&category=DEVELOPER_RELATIONS&category=HARDWARE_ENGINEERING&category=INFORMATION_TECHNOLOGY&category=MANUFACTURING_SUPPLY_CHAIN&category=NETWORK_ENGINEERING&category=PRODUCT_MANAGEMENT&category=PROGRAM_MANAGEMENT&category=SOFTWARE_ENGINEERING&category=TECHNICAL_INFRASTRUCTURE_ENGINEERING&category=TECHNICAL_SOLUTIONS&category=TECHNICAL_WRITING&category=USER_EXPERIENCE&company=Google&company=YouTube&employment_type=INTERN&jex=ENTRY_LEVEL) Стажировка (Стэп) \n" + 
-				"[:cherry_blossom:](https://www.cambridgelms.org/main/p/splash) English \n" 
-				
+				"[:cherry_blossom:](https://www.cambridgelms.org/main/p/splash) English \n" +
+				"[:cherry_blossom:](https://docs.google.com/spreadsheets/d/e/2PACX-1vStDICXS9U_wVcCt3YNecXgXUQtr4A9CWqgUauXMlm52_L8mIbnzXaIYs0sYHH0vatZn2bHZufF_FVr/pubhtml?gid=2143740747&single=true) Расписание\n")
+		   .addField("**_____________**",
+				"[АиСД](http://neerc.ifmo.ru/teaching/algo/year2019/) :cherry_blossom: [Баллы](https://docs.google.com/spreadsheets/d/1q4hb8ZT2XIaUZCylGfglgBkYA1qb9YsVsVJsm6-e0Yw/edit#gid=0)\n" +
+				"[ДМ/дз](http://neerc.ifmo.ru/wiki/index.php?title=%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B9_%D0%BF%D0%BE_%D0%94%D0%9C_2019_%D0%BE%D1%81%D0%B5%D0%BD%D1%8C) :cherry_blossom: [Баллы](https://docs.google.com/spreadsheets/d/e/2PACX-1vQTkyRPeVNCseXvdQ2ct1ZZ0NJw8temN883lXdpoNeMW0KjCkJYBr0CurZneCj2ldq_7b-tzXuWo8qE/pubhtml) \n")
+		   .addField("**_____________**",
+				"[Прога](http://www.kgeorgiy.info//courses/prog-intro/) :cherry_blossom: [Тесты](http://www.kgeorgiy.info/git/geo/prog-intro-2019/) :cherry_blossom: [Баллы](https://docs.google.com/spreadsheets/d/e/2PACX-1vQ52PnrWGnJHzy-KAde38XDw_EoEVBzAfAnHYVb_2Mr0x1LXGwgdXZNuNoA-YO01CA96MGbwu5BhSCL/pubhtml?gid=2006076710&single=true)\n" +
+				"[:cherry_blossom:](https://docs.google.com/spreadsheets/d/1EGAy2292joJBGQtA6ctgDpX7JW0TsHOMMor3FUzN_AU/edit#gid=1255137594) МатАн\n" +
+				"[:cherry_blossom:](https://docs.google.com/spreadsheets/d/1elm_qHxMrXZyE5JvmNjAz7iIruHlV2XYVsglm0DkAnI/htmlview) ЛинАл\n" 				
 			)
 		msg.channel.send(emb);
 	}
